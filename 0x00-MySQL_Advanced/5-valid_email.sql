206a4e02ca4aeca73a5d9c1d65792801e4577cda
Samuelibudi <ibudisamuel@gmail.com>
2023-07-13 07:45:21 +0000

    Fixing errors

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,10 +5,9 @@ CREATE TRIGGER email_trigger
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-IF NEW.email <> OLD.email
-THEN
-	SET NEW.valid_email = 0;
-END IF;
+	IF NEW.email <> OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
 END
 $$
-DELIMITER;
+DELIMITER ;$$
